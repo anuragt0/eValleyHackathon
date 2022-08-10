314680019c42c5c6b333e5f859a44bf1a04e921d
Anurag <anuragtiw112@gmail.com>
2022-08-10 21:50:58 +0530

    input fixes

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -56,8 +56,8 @@ const Signup = () => {
                   <div className="d-flex flex-row align-items-center mb-4">
                     <i className="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div className="form-outline flex-fill mb-0">
-                      <input type="text" id="form3Example1c" className="form-control"  name='name' onChange={onChange}/>
-                      <label className="form-label" htmlFor="form3Example1c">Your Name</label>
+                      <input type="text" id="form3Example1c1" className="form-control"  name='name' onChange={onChange}/>
+                      <label className="form-label" htmlFor="form3Example1c1">Your Name</label>
                     </div>
                   </div>
                   <div className="d-flex flex-row align-items-center mb-4">
@@ -72,8 +72,8 @@ const Signup = () => {
                   <div className="d-flex flex-row align-items-center mb-4">
                     <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                     <div className="form-outline flex-fill mb-0">
-                      <input type="email" id="form3Example3c" className="form-control" name='email' onChange={onChange}/>
-                      <label className="form-label" htmlFor="form3Example3c">Your Email</label>
+                      <input type="email" id="form3Example3c2" className="form-control" name='email' onChange={onChange}/>
+                      <label className="form-label" htmlFor="form3Example3c2">Your Email</label>
                     </div>
                   </div>
 
