400b639267755e3a98598d8444cf849d5f623a6d
Anurag <anuragtiw112@gmail.com>
2022-08-11 09:42:33 +0530

    DB

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-// const mongoURI = process.env.DATABASE;
 const mongoURI = process.env.DATABASE;
+// const mongoURI = "mongodb+srv://eValleyDB:hackathon123@cluster0.irkqaqh.mongodb.net/eValleyDB?retryWrites=true&w=majority";
 
 console.log("hererer: ", mongoURI);
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI).then(()=>{
+const connectToMongo = async ()=>{
+    await mongoose.connect(mongoURI).then(()=>{
         console.log("Connected to Database");
     }).catch((err)=>{console.log("Some error occured in database connection", err);})
 }
